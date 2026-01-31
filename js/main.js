1d8d7bb4a251aff69c2d79af5d94ceeccde3aa3f
zero <zeroziparchives@gmail.com>
2026-01-31 13:39:56 -0600

    add assignment 3 code

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log("hiya, i'm here (〜￣▽￣)〜");
+
+const hours = new Date().getHours();
+
+const isMorning = hours >= 4 && hours < 12;
+const isAfternoon = hours >= 12 && hours < 17;
+const isEvening = hours >= 17 || hours < 4;
+
+const greeting = document.querySelector("#replace");
+if (isMorning) {
+    greeting.textContent = "hello and good morning to you!";
+    }
+else if (isAfternoon) {
+    greeting.textContent = "good afternoon, my friend ☀️";
+    }
+else if (isEvening) {
+    greeting.textContent = "it's awfully late, isn't it?";
+    }
