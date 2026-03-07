f7cfea01ed360893d3cc7df9595f5c82feafc738
zero <zeroziparchives@gmail.com>
2026-03-07 04:16:32 -0600

    add assignment 6 material

--- js/main.js
+++ js/main.js
@@ -12,13 +12,11 @@ const isEvening = hours >= 17 || hours < 4;
 const greeting = document.querySelector("#replace");
 if (isMorning) {
     greeting.textContent = "hello and good morning to you!";
-    }
-else if (isAfternoon) {
+} else if (isAfternoon) {
     greeting.textContent = "good afternoon, my friend ☀️";
-    }
-else if (isEvening) {
+} else if (isEvening) {
     greeting.textContent = "it's awfully late, isn't it?";
-    }
+}
 
 const urls = [
     'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
@@ -26,7 +24,10 @@ const urls = [
     'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-].map(url => { (new Image()).src = url; return url })
+].map(url => {
+    (new Image()).src = url;
+    return url
+})
 
 const images = document.querySelectorAll('#carousel img')
 
@@ -58,3 +59,34 @@ setInterval(() => {
     currentImage++
     showImages()
 }, 5000)
+
+const todoList = document.querySelector('.todo-list');
+const input = document.querySelector('#new-todo');
+const addButton = document.querySelector('#add-button');
+
+let todos = JSON.parse(localStorage.getItem('todo-list')) || [
+    { text: "Buy milk", completed: false },
+    { text: "Walk the dog", completed: false },
+    { text: "Do homework", completed: false }
+];
+
+const renderTodos = () => {
+    todoList.innerHTML = '';
+    todos.forEach(todo => {
+        const li = document.createElement('li');
+        li.textContent = todo.text;
+        todoList.append(li);
+    });
+};
+
+const addNewTodo = () => {
+    if (!input.value) return;
+    todos.push({ text: input.value, completed: false });
+    localStorage.setItem('todo-list', JSON.stringify(todos));
+    input.value = '';
+    renderTodos();
+};
+
+addButton.addEventListener('click', addNewTodo);
+
+renderTodos();
